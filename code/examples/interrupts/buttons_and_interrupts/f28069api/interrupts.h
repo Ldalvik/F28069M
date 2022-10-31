85c1dc921076efe6bfcb088c76b7693922193349
Julian Osborne <subaruforge@gmail.com>
2022-10-31 10:49:04 -0400

    Delete code/examples/interrupts/buttons_and_interrupts directory

--- code/examples/interrupts/buttons_and_interrupts/f28069api/interrupts.h
+++
@@ -1,8 +0,0 @@
-#ifndef F28069API_INTERRUPTS_H_
-#define F28069API_INTERRUPTS_H_
-
-void attachInterrupt(void (*i), int ticks_per_ms);
-void clearInterrupt(int id);
-void stopInterrupt(int id);
-
-#endif /* F28069API_INTERRUPTS_H_ */
