26736cd6be002992ae7bd076270748cc2f6e67bd
Julian Osborne <subaruforge@gmail.com>
2022-10-31 10:50:43 -0400

    Add files via upload

--- code/examples/interrupts/start_stop_interrupts/main.c
+++ code/examples/interrupts/start_stop_interrupts/main.c
@@ -5,62 +5,106 @@
 #include "main.h"
 #include <stdio.h>
 
-#define TICKS_PER_MS 10 // How many ticks a cycle
+#define TICKS_PER_MS 10
 
-#define LED_BLINK_PERIOD (250 * TICKS_PER_MS) 
+#define LED_BLINK_PERIOD (500 * TICKS_PER_MS)
 #define NEVER_16 0xFFFF
 
-// Timers for LED blinking
-volatile uint16_t LedTimer = 0;
+volatile long Timer0 = 0, Timer1 = 0, Timer2 = 0;
 
-static interrupt void TimerInterrupt()
+interrupt void Timer0Interrupt()
 {
     clearInterrupt(0);
-    if(LedTimer < (NEVER_16-1))
+    if(Timer0 < (NEVER_16-1))
     {
-        LedTimer++;
+        Timer0++;
     }
 }
 
-#define button 76
+interrupt void Timer1Interrupt()
+{
+    clearInterrupt(1);
+    if(Timer1 < (NEVER_16-1))
+    {
+        Timer1++;
+    }
+}
+
+interrupt void Timer2Interrupt()
+{
+    clearInterrupt(2);
+    if(Timer2 < (NEVER_16-1))
+    {
+        Timer2++;
+    }
+}
+
+#define BTN1 75
+#define BTN2 76
+
+#define DEBUG_WIRE 38
+
+#define LED1 78
+#define LED2 79
+#define LED3 40
 
 void setup()
 {
-		// Set button as input
-		pinMode(button, INPUT);
-		// Set onboard red/blue LEDs as output
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+
     pinMode(RED_LED, OUTPUT);
     pinMode(BLUE_LED, OUTPUT);
-    digitalWrite(BLUE_LED, HIGH); // Set blue LED initial state to on
 
-    InitInterrupts(); // Initialize registers for interrupts
+    pinMode(BTN1, INPUT);
+    pinMode(BTN2, INPUT);
+
+    pinMode(DEBUG_WIRE, INPUT);
+
+    InitInterrupts();
 }
 
 void loop()
 {
-		static int lastButtonState = 0;
+    int b1 = digitalRead(BTN1);
+    static int lastButtonState = 0, state = 0;
 
-    if (LedTimer > LED_BLINK_PERIOD) // Toggle every 250ms
+    if (b1 != lastButtonState)
     {
-        LedTimer = 0; // Reset timer
-        togglePin(RED_LED);
-        togglePin(BLUE_LED);
+        if (b1)
+        {
+            if (state == 0)
+            {
+                attachInterrupt(&Timer0Interrupt, TICKS_PER_MS);
+            }
+            if (state == 1)
+            {
+                attachInterrupt(&Timer1Interrupt, TICKS_PER_MS);
+            }
+            if (state == 2)
+            {
+                attachInterrupt(&Timer2Interrupt, TICKS_PER_MS);
+            }
+            state++;
+        }
+        lastButtonState = b1;
+    }
+    if (Timer0 > LED_BLINK_PERIOD)
+    {
+        Timer0 = 0;
+        togglePin(LED1);
+    }
+    if (Timer1 > LED_BLINK_PERIOD)
+    {
+        Timer1 = 0;
+        togglePin(LED2);
     }
 
-    if (button != lastButtonState)
+    if (Timer2 > LED_BLINK_PERIOD)
     {
-        if (button)
-        {
-            if (CpuTimer1Regs.TCR.bit.TIE == 0) // Check if CpuTimer is unused. Disable it if enabled.
-            {
-                attachInterrupt(&TimerInterrupt, TICKS_PER_MS);
-            }
-            else
-            {
-                stopInterrupt(0);
-            }
-        }
-        lastButtonState = button
+        Timer2 = 0;
+        togglePin(LED3);
     }
 }
 
